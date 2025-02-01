433f3c680f6c9025a7d14a922ce34da67cdcd42b
Miguel Bastos <miguel.alexandre.bastos@gmail.com>
2025-02-01 14:55:05 +0000

    Finished first iteration

--- src/add.js
+++ src/add.js
@@ -2,6 +2,10 @@ function add (numOne, numTwo) {
     if (numOne == undefined || numTwo == undefined) {
         return undefined;
     }
+     if (numOne == NaN || numTwo == NaN){
+        return undefined;
+    }
+    
 
     return numOne + numTwo;
 }
--- tests/add.spec.js
+++ tests/add.spec.js
@@ -19,6 +19,12 @@ describe("Iteration 1 | Getting Started", () => {
         expect(add()).toEqual(undefined);
         expect(add(undefined, 1)).toEqual(undefined);
       });
+
+      it("should return undefined if any of the arguments is NaN", () =>{
+        expect(add(1, NaN)).toEqual(NaN)
+        // expect(add()).toEqual(NaN)
+        expect(add(NaN, 1)).toEqual(NaN)
+      })
     });
   });
   
